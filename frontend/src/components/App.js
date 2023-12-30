44bce140174f65342bf2e62d3a5554717bea6639
lamvd <vuduclam2412@gmail.com>
2023-12-30 16:22:27 +0700

    Use Routes instead Switch react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
-import Nav from "./Nav.jsx"
+import { Routes, Route } from "react-router-dom";
+import Nav from "./Nav.jsx";
 import Home from "../pages/Home.jsx";
 import Auth from "../pages/Auth.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 
 export const App = (props) => {
-  return <>
-  <Nav/>
-  <Switch>
-    <Router exact path="/" component={Home} />
-    <Router path="/auth/:form" componnent={Auth} />
-    <Router path="/dashboard" componnent={Dashboard} />
-  </Switch></>;
+  return (
+  <>
+    <Nav />
+    <Routes>
+      <Route exact path="/" component={Home} />
+      <Route path="/auth/:form" component={Auth} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Routes>
+  </>
+  );
 };
